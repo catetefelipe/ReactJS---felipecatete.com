b5a1fd556af986e3342d7216ad572fbf11548b32
Felipe Catete <83040279+felipe-catete@users.noreply.github.com>
2021-06-15 09:08:39 -0300

    Add files via upload

--- src/Components/Goals.js
+++ src/Components/Goals.js
@@ -14,7 +14,7 @@ function Goals() {
 
                 <div className="goals_text_div">
                 <h2>Objetivos</h2>
-                <p>Quero ajudar a minha equipe com a minha energia e com o máximo do meu conhecimento, buscando solucionar problemas, ajudando o próximo e consequentemente evoluindo como programador. Entre em contato comigo, vou ficar muito feliz com o seu interesse no meu trabalho!</p>
+                <p>Quero ajudar a minha equipe com a minha energia e com o máximo do meu conhecimento, buscando solucionar problemas, ajudando o próximo e consequentemente evoluindo como programador.</p>
                 </div>
             </div>
         </section>
