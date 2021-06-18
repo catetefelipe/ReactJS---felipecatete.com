a3e0f5d5810390bf50a6a8ca2b35fbab0170761b
Felipe Catete <83040279+felipe-catete@users.noreply.github.com>
2021-06-18 02:04:14 -0300

    Add files via upload

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -12,7 +12,7 @@ function AboutMe() {
                 <div className="aboutme_img_div">
                     <img src={pic} alt="Felipe Catete" />
                 </div>
-                <p>Estou em busca da evolução na minha carreira como programador, por isso procuro por uma empresa com profissionais motivados, que em troca receberão toda a minha dedicação e ajuda.</p>
+                <p>Estou em busca da evolução na minha carreira como programador, por isso procuro por uma empresa com profissionais motivados que em troca receberão toda a minha dedicação e ajuda.</p>
             </div>
         </section>
     )
